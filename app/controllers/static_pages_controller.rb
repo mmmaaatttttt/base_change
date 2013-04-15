ffa566fd099ecf65429a21c71f730d9f86ca7987
Matt Lane <lane.matthew@gmail.com>
2013-04-15 10:50:37 -0700

    Add a StaticPageages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def exemplars
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 BaseChange::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/exemplars"
+
+  get "static_pages/blog"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
